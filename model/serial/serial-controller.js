3091ebdb29337f9a68be42c8d4aabf6dfcdc4d31
kostia777999 <kostya.gorozhanov@mail.ru>
2017-02-20 23:49:03 +0200

    search by serialGroup

--- model/serial/serial-controller.js
+++ model/serial/serial-controller.js
@@ -21,7 +21,11 @@ class SerialController extends Controller {
         let query = {};
         if (req.query) {
             for (let key in req.query) {
-                query[key] = new RegExp('^' + req.query[key]);
+                if(req.query[key] === 'serialGroup') {
+                    query[key] = req.query[key];
+                } else {
+                    query[key] = new RegExp('^' + req.query[key]);
+                }
             }
         }
         return this.model.paginate(query, options)
