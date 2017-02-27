70c5ec434858ea624c18d88fd6d7f2bd9f53619f
kostia777999 <kostya.gorozhanov@mail.ru>
2017-02-27 20:27:43 +0200

    change password method

--- model/user/user-router.js
+++ model/user/user-router.js
@@ -7,8 +7,8 @@ const router = new Router();
 router.get('/', auth.hasRole('admin'), controller.index);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 router.get('/me', auth.isAuthenticated(), controller.me);
-router.put('/:id', auth.isAuthenticated(), controller.updateUser);
-router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/', auth.hasRole('admin'), controller.create);
-
+// router.put('/:id', auth.isAuthenticated(), controller.updateUser);
+// router.get('/:id', auth.isAuthenticated(), controller.show);
+// router.post('/', auth.hasRole('admin'), controller.create);
+router.post('/password', auth.isAuthenticated(), controller.changePassword);
 module.exports = router;
